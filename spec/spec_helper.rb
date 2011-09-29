37b03931ffd86317cb2efeec59aee1b7c571e3c8
josh <joshhattan@gmail.com>
2011-09-28 18:00:36 -0600

    spec_helper update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,13 +6,13 @@ Spork.prefork do
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
   ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/envirionment", __FILE__)
+  require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  
+
   # Requires supporting files with custom matchers and macros, etc,
   # in ./support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-  
+
   RSpec.configure do |config|
     # == Mock Framework
     #
@@ -22,13 +22,13 @@ Spork.prefork do
     # config.mock_with :flexmock
     # config.mock_with :rr
     config.mock_with :rspec
-    
+
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
-    
+
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, comment the following line or assign false
     # instead of true.
-    config.use_transactional_fixtures = true        
+    config.use_transactional_fixtures = true
   end
 end
 
