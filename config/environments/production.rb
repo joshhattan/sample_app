14f66a2e1145eca79db1c2436dc830d25635c6e4
josh <joshhattan@gmail.com>
2011-10-02 19:17:56 -0600

    changed .css manifest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ SampleApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   #config.assets.precompile += %w( 'blueprint/screen.css', 'blueprint/print.css', 'blueprint/ie.css' )
-  config.assets.precompile += %w( '*.css', '*.js' )
+  #config.assets.precompile += %w( '*.css', '*.js' )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
