7bff8209348f8214047754706df0ed4a99126a8c
HTPC <joshhattan@gmail.com>
2011-10-05 18:41:08 -0600

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+    user.name                  "Josh Hattan"
+    user.email                 "joshhattan@gmail.com"
+    user.password              "foobar"
+    user.password_confirmation "foobar"
+end
